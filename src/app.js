f37cd6d5f1eeecd6f2efcb6c6bc9763d78ee6447
Elina Kayumova <129918798+elinakay@users.noreply.github.com>
2023-12-01 14:53:46 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const errorHandler = require("./errors/errorHandler");
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://movies-frontend-csfo.onrender.com');
+  res.header('Access-Control-Allow-Origin', 'https://movies-frontend-csfo.onrender.com/');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
